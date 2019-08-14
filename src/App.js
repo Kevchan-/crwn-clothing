0791a19b445be82b9769102415dbe06263138256
Kevin folgar <kevinfb92@gmail.com>
2019-08-14 11:45:47 +0200

    firebase authentication and firestore document creation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,41 @@ import HomePage from './pages/homepage/homepage.component'
 import {Switch, Route} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sing-up/sign-in-and-sing-up.component';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-    <Header/>
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
-    </Switch>      
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsuscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async user=>{
+      createUserProfileDocument(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsuscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser}/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
+      </Switch>      
+      </div>
+    )
+  }
 }
 
 export default App;
